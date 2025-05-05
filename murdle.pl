62d1836523fa9b3724b636b1dec04a8d445786e3
Michelle Hughes <michelle@iheartmichelle.com>
2025-05-05 00:03:39 -0400

    "The tallest suspect".

--- murdle.pl
+++ murdle.pl
@@ -1,3 +1,23 @@
+% Reasoning about height.
+% left_taller([F1, I1], [F2, I2]) :- (F1 > F2); (F1=:=F2, I1 > I2).
+% same_height([F1, I1], [F2, I2]) :- F1 =:= F2, I1 =:= I2.
+% left_shorter([F1, I1], [F2, I2]) :- (F1 < F2); (F1=:=F2, I1 < I2).
+
+left_taller(P1, P2) :- 
+  height(P1, [F1, I1]), height(P2, [F2, I2]), (F1 > F2);
+  height(P1, [F1, I1]), height(P2, [F2, I2]), F1==F2, I1 > I2.
+
+same_height(P1, P2) :- height(P1, [F1, I1]), height(P2, [F2, I2]), F1 == F2, I1 == I2.
+
+left_shorter(P1, P2) :- 
+  height(P1, [F1, I1]), height(P2, [F2, I2]), (F1 < F2);
+  height(P1, [F1, I1]), height(P2, [F2, I2]), F1==F2, I1 < I2.
+
+
+tallest([P|[]], P).
+tallest([P|[P1|T]], M) :- tallest([P|T], M), left_taller(P, P1); tallest([P|T], M), same_height(P, P1); tallest([P1|T], M), left_shorter(P, P1).
+
+
 % Biographical Info
 % I wonder if this information is always the same for each of these people.
 % I'll add to this as each day's clues make the info relevant.  Eventually,
@@ -11,6 +31,14 @@ right_handed(babyface_blue).
 right_handed(silverton_the_legend).
 left_handed(signor_emerald).
 left_handed(mx_tangerine).
+
+height(president_amaranth, [5,10]).
+height(vice_president_mauve, [5,8]).
+height(babyface_blue, [7,8]).
+height(silverton_the_legend, [6,4]).
+height(signor_emerald, [5,8]).
+height(mx_tangerine, [5,5]).
+
 
 % Weapon data
 medium_weight(bottle).
@@ -55,7 +83,7 @@ solution(L) :-
   member([silverton_the_legend, _, theater], L),
   member([_, cufflinks, guard_tower], L),
   member([signor_emerald, _, rec_room], L), % The suspect with the same height as vice president mauve
-  member([babyface_blue, _, private_suite], L), % The tallest suspect
+  member([Tallest, _, private_suite], L), findall(P2, height(P2, _), People), tallest(People, Tallest),
   member([mx_tangerine, key, _], L),
   member([_, cake, rec_room], L),
   member([president_amaranth, W1, _], L), medium_weight(W1),
